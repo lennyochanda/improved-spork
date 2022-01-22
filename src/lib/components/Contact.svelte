c572aaeba2ed13e7877d8c68fe9a27aea253a3f6
dak-384 <dakkofficial@gmail.com>
2022-01-22 19:13:56 +0300

    [fix] form data not sending

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -19,19 +19,20 @@
 <Callout>Any suggestions, corrections, questions or greetings are welcome below. Find me on <a href="https://www.twitter.com/lenny_ochanda">Twitter</a> for the quickest reply.</Callout>
 
 <form id="contact" name="contact" method="POST" netlify data-netlify="true" netlify-honeypot="bot-field">
+  <input type="hidden" name="form-name" value="contact">
   <div class="form-section">
     <label for="name">Name</label>
-    <input type="text" id="name" name="name" placeholder="Name" />
+    <input type="text" id="name" name="name" placeholder="Name" value="" />
   </div>
   
   <div class="form-section">
     <label for="email">Email</label>
-    <input type="email" id="email" name="email" placeholder="address@something.com" />
+    <input type="email" id="email" name="email" placeholder="address@something.com" value="" />
   </div>
 
   <div class="form-section">
     <label for="message">Message</label>
-    <textarea id="message" name="message" placeholder="Hi, Lenny..." max-length="250"></textarea>
+    <textarea id="message" name="message" placeholder="Hi, Lenny..." value="" max-length="250"></textarea>
   </div>
 
   <button type="submit">Send!</button>
