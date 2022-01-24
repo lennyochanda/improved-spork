bdafe5512005ed135abce3dc79916fc4fc07c728
dak-384 <dakkofficial@gmail.com>
2022-01-24 18:30:59 +0300

    Add ProjectCard component styles

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -3,28 +3,44 @@
 </script>
 
 {#if project}
-  <div>
-    <div>
-      <h3>
-        <a href={project.url}>{project.title}</a>
-      </h3>
-    </div>
+  <div class="project-card">
+    <h3>
+      <a href={project.url.live}>{project.title}</a>
+    </h3>
     <div>
     	<img
         alt="{project.alt}"
         src="{project.coverImage}"
-        width="1.75rem"
-        height="1.75rem"
+        width="30px"
+        height="50px"
       />
             
-      <p>
-        {project.description}
-      </p>
-    </div>
     <div>
-      {#each project.stack as stack}
-        <span>{stack}</span>
-      {/each}
+      <ul>
+        {#each project.stack as stack}
+          <a href="/blog/category/{stack}" class="stack">{stack}</a>
+        {/each}
+      </ul>
     </div>
+    <p>
+      {project.description}
+    </p>
+    </div>
+    <ul class="links">
+      <a href="{project.url.live}" target="_blank">Live</a>
+      <a href="{project.url.source}" target="_blank">Source</a>
+    </ul>
   </div>
 {/if}
+
+<style>
+  ul {
+    display: inline-flex;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0;
+  }
+  .links {
+    justify-content: space-between;
+  }
+</style>
